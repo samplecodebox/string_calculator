f921fc005490973015dc78fda603b1a22a40bd69
= <=>
2012-01-18 12:42:09 -0800

    string calculator

--- lib/main.rb
+++ lib/main.rb
@@ -4,5 +4,14 @@
 require 'string_calculator'
 
 calculator = StringCalculator.new
-result = calculator.add("")
-puts "Calucator Returns #{result}"
+puts "Please enter the string to be calculated"
+input_string = gets
+is_valid = calculator.validate(input_string)
+if(is_valid)
+  operand_list = calculator.parse(input_string)
+  result = calculator.add(operand_list)
+  puts "Result: #{result}"
+else
+  puts "Invalid String"
+end
+
--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,22 +3,37 @@
 
 class StringCalculator
 
-  def initialize
-    @sum = 0
+  # method adds the operands passed after parsing the string and returns the added result
+  def add(operand_list)
+      sum = 0
+      operand_list.each do |operand|
+        sum = sum + operand.to_i
+      end
+      sum
   end
 
-  def add(input_string)
-    operand_list = parser(input_string)
-    operand_list.each do |operand|
-      @sum+= operand.to_i
+  # validates the input string
+    # - if the string contains negative numbers
+    # - if the string has two delimiters adjacent to each other and if there is any invalid character
+    # - checks for user defined delimiter
+  def validate(input_string)
+    check_negative_number = input_string.scan(/-\d/)
+    if check_negative_number.empty?
+      different_delimitor_pattern = /^\/\/[;:&]?[\n\\n]/ # pattern matching different delimitors. Right now added only ;, : and & as valid delimitor
+      if(!input_string.match(different_delimitor_pattern).nil?)
+        new_delimitor = input_string[2,1]
+        input_string.gsub!(different_delimitor_pattern, "").gsub!(new_delimitor, ",")
+      end
+      check_for_invalid_characters = input_string.match(/(\d+,|\d+\n|\d+\\n)*(\d*)/)
+      input_string.eql?(check_for_invalid_characters.to_s) ? true : false
+     else
+      puts "Negative number not allowed. And #{check_negative_number.join(",")} has been passed"
     end
-    result = "Result: #{@sum}"
-    result
   end
 
-  private
-  def parser(input_string)
-      operand_list = input_string.split(",")
-      operand_list
+  # parses the input string and returns the list of operands for the add method
+  def parse(input_string)
+    operand_list = input_string.split(/[,|\n|\\n]/)
+    operand_list
   end
 end
